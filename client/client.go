0dca01b251a2080643b56d01afa75341db5406bb
rusik <rusik@4ege.ru>
2018-09-26 22:47:34 +0200

    commit

---
+++ client/client.go
@@ -0,0 +1,196 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+const hostname = "http://localhost:8888"
+
+func storageUpload(name string) error {
+	dat, err1 := ioutil.ReadFile(name)
+	if err1 != nil {
+		fmt.Println(err1)
+		panic("file read error")
+	}
+	url := fmt.Sprintf("%s/upload/%s", hostname, name)
+	body := bytes.NewBuffer(dat)
+	dat2, err2 := http.Post(url, "application/octet-stream", body)
+	if err2 != nil {
+		fmt.Println(err2)
+		panic("upload error")
+		return err2
+	}
+	if dat2.StatusCode != 200 {
+		fmt.Println(dat2.StatusCode)
+		panic("status code error")
+		return http.ErrServerClosed
+	}
+	return nil
+}
+
+func storageDownload(name string) error {
+	url := fmt.Sprintf("%s/download/%s", hostname, name)
+	fmt.Println(url)
+	dat, err1 := http.Get(url)
+	if err1 != nil {
+		fmt.Println(err1)
+		panic("get error")
+	}
+	if dat.StatusCode != 200 {
+		fmt.Println(dat.StatusCode)
+		panic("status code error")
+	}
+	bodyBytes, err2 := ioutil.ReadAll(dat.Body)
+	if err2 != nil {
+		fmt.Println(err2)
+		panic("body read error")
+	}
+	err3 := ioutil.WriteFile(name, []byte(bodyBytes), 0644)
+	if err3 != nil {
+		fmt.Println(err3)
+		panic("file write error")
+	}
+	return nil
+}
+
+func storageList() error {
+	url := fmt.Sprintf("%s/list", hostname)
+	fmt.Println(url)
+	dat, err1 := http.Get(url)
+	if err1 != nil {
+		panic("get error")
+	}
+	if dat.StatusCode != 200 {
+		fmt.Println(dat.StatusCode)
+		panic("status code error")
+	}
+	bodyBytes, err2 := ioutil.ReadAll(dat.Body)
+	if err2 != nil {
+		fmt.Println(err2)
+		panic("body read error")
+	}
+	fmt.Println(bodyBytes)
+	return nil
+}
+
+func storageHelp() {
+	fmt.Println("upload, download or list")
+}
+
+func storage(action, name string) {
+	if name == "" {
+		storageHelp()
+		return
+	}
+	switch action {
+	case "help":
+		storageHelp()
+	case "upload":
+		err := storageUpload(name)
+		if err != nil {
+			fmt.Println(err)
+			panic("storage upload failure")
+		}
+	case "download":
+		err := storageDownload(name)
+		if err != nil {
+			fmt.Println(err)
+			panic("storage download failure")
+		}
+	case "list":
+		err := storageList()
+		if err != nil {
+			fmt.Println(err)
+			panic("storage list failure")
+		}
+	}
+}
+
+func req(actionType, method, name, id string) ([]byte, error) {
+	url := ""
+	if name != "" {
+		// http://localhost:8888/run/redis
+		url = fmt.Sprintf("%s/%s/%s/%s", hostname, actionType, name, id)
+	} else {
+		// http://localhost:8888/stop/redis/ID
+		url = fmt.Sprintf("%s/%s/%s", hostname, actionType, name)
+	}
+	fmt.Print(url)
+	var err1 error
+	var resp *http.Response
+	if method == "post" {
+		body := bytes.NewBuffer([]byte(""))
+		resp, err1 = http.Post(url, "application/octet-stream", body)
+		defer resp.Body.Close()
+	} else {
+		resp, err1 = http.Get(url)
+	}
+	if err1 != nil {
+		fmt.Println(err1)
+		panic("post error")
+	}
+	if resp.StatusCode != 200 {
+		fmt.Println(resp.StatusCode)
+		panic("post error")
+	}
+	b, err2 := ioutil.ReadAll(resp.Body)
+	if err2 != nil {
+		fmt.Println(err2)
+		panic("response read error")
+	}
+	return b, nil
+}
+
+func printHelp() {
+	fmt.Println("storage, mysql or redis")
+}
+
+func main() {
+	if len(os.Args) > 1 {
+		command := os.Args[1]
+		switch command {
+		case "help":
+			printHelp()
+		case "storage":
+			if len(os.Args) < 4 {
+				if len(os.Args) > 2 {
+					storage(os.Args[2], "")
+				} else {
+					fmt.Println("upload, download or list and filename")
+				}
+			} else {
+				storage(os.Args[2], os.Args[3])
+			}
+		case "mysql":
+			if len(os.Args) < 4 {
+				if len(os.Args) > 2 {
+					fmt.Println("run, list or stop")
+					return
+				}
+				req("mysql", "get", os.Args[2], "")
+			} else {
+				req("mysql", "post", os.Args[2], os.Args[3])
+			}
+		case "redis":
+			if len(os.Args) < 4 {
+				if len(os.Args) > 2 {
+					fmt.Println("run, list or stop")
+					return
+				}
+				req("redis", "get", os.Args[2], "")
+			} else {
+				req("redis", "post", os.Args[2], os.Args[3])
+			}
+		default:
+			printHelp()
+
+		}
+	} else {
+		printHelp()
+		os.Exit(1)
+	}
+}
---
+++ server/server.go
@@ -0,0 +1,179 @@
+package main
+
+import (
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"golang.org/x/net/context"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+
+const addr = "localhost:8888"
+const dataDir = "data"
+
+func storageUploadHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("storage upload")
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("request reading error")
+		return
+	}
+	pathSplit := strings.Split(r.URL.Path, "/")
+	filename := fmt.Sprintf("%s, %s", dataDir, pathSplit[len(pathSplit)-1])
+	err3 := ioutil.WriteFile(filename, []byte(body), 0644)
+	if err3 != nil {
+		fmt.Println("file write error")
+		fmt.Println(err3)
+		return
+	}
+	fmt.Println("file %s uploaded", filename)
+}
+
+func storageDownloadHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("storage download")
+	pathSplit := strings.Split(r.URL.Path, "/")
+	filename := pathSplit[len(pathSplit)-1]
+	dat, err1 := ioutil.ReadFile(fmt.Sprintf("data/%s", filename))
+	if err1 != nil {
+		fmt.Println("file read error: " + filename)
+		fmt.Println(err1)
+		return
+	}
+	_, err2 := w.Write(dat)
+	if err2 != nil {
+		fmt.Println("request write error")
+		fmt.Println(err2)
+	}
+	fmt.Println("file %s downloaded", filename)
+}
+
+func storageListHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("list")
+	files, err := ioutil.ReadDir(dataDir)
+	if err != nil {
+		fmt.Println("dir list error")
+		fmt.Println(err)
+	}
+	var l []string
+	for _, f := range files {
+		l = append(l, f.Name())
+	}
+	s := strings.Join(l, "\n")
+	w.Write([]byte(s))
+}
+
+func listContainers(typeName string) string {
+	c := client.WithVersion("1.38")
+	cli, err := client.NewClientWithOpts(c)
+	if err != nil {
+		fmt.Println("client create error")
+		fmt.Println(err)
+		return ""
+	}
+	var l []string
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		fmt.Println("containerList error")
+		fmt.Println(err)
+		return ""
+	}
+	for _, c := range containers {
+		l = append(l, c.ID)
+	}
+	s := strings.Join(l, "\n")
+	return s
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	pathSplit := strings.Split(r.URL.Path, "/")
+	typeName := pathSplit[len(pathSplit)-1]
+	s := listContainers(typeName)
+	w.Write([]byte(s))
+}
+
+func runContainer(imageName string) (string, error) {
+	ctx := context.Background()
+	c := client.WithVersion("1.38")
+	cli, err := client.NewClientWithOpts(c)
+	if err != nil {
+		fmt.Println("client create error")
+		fmt.Println(err)
+		return "", err
+	}
+	out, err2 := cli.ImagePull(ctx, imageName, types.ImagePullOptions{})
+	if err2 != nil {
+		fmt.Println("image pull error")
+		fmt.Println(out)
+		return "", err2
+	}
+	resp, err3 := cli.ContainerCreate(ctx, &container.Config{
+		Image: imageName,
+	}, nil, nil, "")
+	if err3 != nil {
+		fmt.Println("container create error")
+		fmt.Println(resp)
+		return "", err3
+	}
+	err4 := cli.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{})
+	if err4 != nil {
+		fmt.Println("container start error")
+		return "", err4
+	}
+	return resp.ID, nil
+}
+
+func stopContainer(containerId string) error {
+	ctx := context.Background()
+	c := client.WithVersion("1.38")
+	cli, err1 := client.NewClientWithOpts(c)
+	if err1 != nil {
+		fmt.Println("client create error")
+		fmt.Println(err1)
+		return err1
+	}
+	err2 := cli.ContainerStop(ctx, containerId, nil)
+	if err2 != nil {
+		fmt.Println("container stop error")
+		fmt.Println(err2)
+		return err2
+	}
+	return nil
+}
+
+func runHandler(w http.ResponseWriter, r *http.Request) {
+	pathSplit := strings.Split(r.URL.Path, "/")
+	imageName := pathSplit[len(pathSplit)-1]
+	id, err := runContainer(imageName)
+	if err == nil {
+		fmt.Fprintf(w, id)
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, err.Error())
+	}
+}
+
+func stopHandler(w http.ResponseWriter, r *http.Request) {
+	pathSplit := strings.Split(r.URL.Path, "/")
+	containerId := pathSplit[len(pathSplit)-1]
+	err := stopContainer(containerId)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, err.Error())
+	}
+}
+
+func main() {
+	fmt.Println("starting server")
+	http.HandleFunc("/upload/", storageUploadHandler)
+	http.HandleFunc("/download/", storageDownloadHandler)
+	http.HandleFunc("/list", storageListHandler)
+	http.HandleFunc("/run/", runHandler)
+	http.HandleFunc("/stop/", stopHandler)
+	http.HandleFunc("/list/", listHandler)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		panic(err)
+	}
+}
