1d1e95b342f337c1b6da6d0f40c54609dc208405
Ruslan Gustomiasov <rusik@4ege.ru>
2019-07-22 00:20:38 +0200

    run deployment in parallel

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -x
 set -e
+start=`date +%s`
 CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o ../build/package/server ../cmd/server
 cd ../
 docker build --no-cache -f build/package/Dockerfile . -t rws
@@ -17,13 +18,20 @@ etcdctl mkdir /rws/pods
 etcdctl mkdir /rws/containers
 etcdctl mkdir /rws/storage
 docker-compose -f docker-compose.yml up -d
-for i in $(seq 2 5); do
-    scp docker-compose.yml pi$i:~/
-    ssh pi$i docker-compose -f docker-compose.yml down --remove-orphans
-    ssh pi$i docker pull loqutus/rws
-    ssh pi$i docker-compose -f docker-compose.yml up -d
-done
+s(){
+    scp docker-compose.yml pi$1:~/
+    ssh pi$1 docker-compose -f docker-compose.yml down --remove-orphans
+    ssh pi$1 docker pull loqutus/rws
+    ssh pi$1 docker-compose -f docker-compose.yml up -d
+}
+s 2 &
+s 3 &
+s 4 &
+s 5 &
+wait
 sleep 2
 cd ../cmd/client
 go test
+end=`date +%s`
+echo $((end-start))
 docker logs -f deployments_rws_1
